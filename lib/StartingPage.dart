870d8d379086d1d81a4ae4cbb962882b4126ddc5
Antoni <antekkrol1@gmail.com>
2022-03-24 09:25:18 +0100

    Starting Page added

---
+++ lib/StartingPage.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+import 'package:io_project/HomePage.dart';
+
+class startingPage extends StatefulWidget {
+  const startingPage({Key? key}) : super(key: key);
+
+  @override
+  State<startingPage> createState() => _startingPageState();
+}
+
+class _startingPageState extends State<startingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "FitApp",
+          style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              shadows: [
+                Shadow(
+                    color: Colors.black54, offset: Offset(1, 2), blurRadius: 4)
+              ]),
+        )),
+        elevation: 0,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(5),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade200,
+                offset: const Offset(2, 4),
+                blurRadius: 5,
+                spreadRadius: 2,
+              ),
+            ],
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.lightBlue,
+                Colors.blue,
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                  ),
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xff42a5f5),
+                        offset: Offset(
+                          2,
+                          4,
+                        ),
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                    color: Colors.white,
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(
+                        0xFF0389F6,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 13,
+                  ),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(
+                        15,
+                      ),
+                    ),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  child: const Text(
+                    'Register now',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,168 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:io_project/profile_page.dart';
 
-import 'ProfileScreen.dart';
-import 'package:io_project/profile_page.dart';
+import 'package:io_project/StartingPage.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key:key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
-      home:HomePage(),
-    );
-  }
-}
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  //Firebase init
-  Future<FirebaseApp> _initializeFirebase() async{
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-    return firebaseApp;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: FutureBuilder(
-          future: _initializeFirebase(),
-          builder: (context, snapshot){
-            if(snapshot.connectionState == ConnectionState.done){
-              return LoginScreen();
-            }
-            return const Center(
-                child: CircularProgressIndicator(),
-            );
-          },
-        ),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-
-  //login function
-  static Future<User?> loginUsingEmailPassword({
-    required String email,
-    required String password,
-    required BuildContext context}) async{
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
-    try{
-      UserCredential userCredential = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      user = userCredential.user;
-    }
-    on FirebaseAuthException catch (e){
-      if(e.code == "user-not-found"){
-        print("No User found for this email adress");
-      }
-    }
-    return user;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _passwordController = TextEditingController();
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-                "KittelKrol",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,)
-            ),
-            const Text("Let's start the workout !!!",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 42.0,
-                  fontWeight: FontWeight.bold,
-                )
-            ),
-            const SizedBox(
-              height: 44.0,
-            ),
-            TextField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: "e-mail",
-                prefixIcon: Icon(Icons.mail,color: Colors.black),
-              ),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: "Password",
-                prefixIcon: Icon(Icons.security,color: Colors.black),
-              ),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            const Text("Forgot your password?",style:TextStyle(color:Color(0xFF817DC0))),
-            const SizedBox(
-              height: 18.0,
-            ),
-            Container(
-                width: double.infinity,
-                child: RawMaterialButton(
-                  fillColor: const Color(0xFFC7B8F5),
-                  elevation: 0.0,
-                  padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusDirectional.circular(12.0)
-                  ),
-                  onPressed: () async {
-                    User? user = await loginUsingEmailPassword(email: _emailController.text, password: _passwordController.text, context: context);
-                    print(user);
-                    if(user != null) {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ProfilePage()));
-                    }
-                    else{
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ProfileScreen()));
-                    }
-                  },
-                  child: const Text("LOGIN",
-                    style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,),),)
-            )
-          ],
-      ),
+      home: startingPage(),
     );
   }
 }
