67659f49495877d33a25d6d9a5c963453800c884
Antoni <antekkrol1@gmail.com>
2022-03-24 12:20:16 +0100

    :D

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:io_project/profile_page.dart';
 import 'ProfileScreen.dart';
-import 'package:io_project/profile_page.dart';
+import 'package:io_project/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -70,86 +69,109 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     TextEditingController _emailController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("KittelKrol",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              )),
-          const Text("Let's start the workout !!!",
-              style: TextStyle(
-                fontFamily: 'Cairo',
-                color: Colors.black,
-                fontSize: 42.0,
-                fontWeight: FontWeight.bold,
-              )),
-          const SizedBox(
-            height: 44.0,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(5),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            offset: const Offset(2, 4),
+            blurRadius: 5,
+            spreadRadius: 2,
           ),
-          TextField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: "e-mail",
-              prefixIcon: Icon(Icons.mail, color: Colors.black),
-            ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: "Password",
-              prefixIcon: Icon(Icons.security, color: Colors.black),
-            ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text("Forgot your password?",
-              style: TextStyle(color: Color(0xFF817DC0))),
-          const SizedBox(
-            height: 18.0,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: const Color(0xFFC7B8F5),
-                elevation: 0.0,
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusDirectional.circular(12.0)),
-                onPressed: () async {
-                  User? user = await loginUsingEmailPassword(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                      context: context);
-                  print(user);
-                  if (user != null) {
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => ProfilePage()));
-                  } else {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => ProfileScreen()));
-                  }
-                },
-                child: const Text(
-                  "LOGIN",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ))
         ],
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            mBackgroundColor,
+            Color(0xFF817DC0),
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("FitApp",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            const Text("Let's start the workout !!!",
+                style: TextStyle(
+                  fontFamily: 'Cairo',
+                  color: Colors.black,
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            const SizedBox(
+              height: 44.0,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "e-mail",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.security, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const Text("Forgot your password?",
+                style: TextStyle(color: Color(0xFF817DC0))),
+            const SizedBox(
+              height: 18.0,
+            ),
+            SizedBox(
+                width: double.infinity,
+                child: RawMaterialButton(
+                  fillColor: const Color(0xFFC7B8F5),
+                  elevation: 0.0,
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusDirectional.circular(12.0)),
+                  onPressed: () async {
+                    User? user = await loginUsingEmailPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                        context: context);
+                    print(user);
+                    if (user != null) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => ProfilePage()));
+                    } else {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => ProfileScreen()));
+                    }
+                  },
+                  child: const Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
