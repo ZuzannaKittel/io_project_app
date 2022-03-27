7ce1f5f18232161f3ba7601cddab3e2722bb0dc9
Antoni <antekkrol1@gmail.com>
2022-03-28 00:31:28 +0200

    Cos tam ze zdjeciem

--- lib/EditProfilePage.dart
+++ lib/EditProfilePage.dart
@@ -1,12 +1,17 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:io_project/widget/bottom_nav_bar.dart';
 import 'package:io_project/model/user.dart';
 import 'package:io_project/utils/user_preferences.dart';
 import 'package:io_project/widget/appbar_widget.dart';
 import 'package:io_project/widget/profile_widget.dart';
 import 'package:io_project/widget/textfield_widget.dart';
+import 'package:path/path.dart';
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
@@ -17,7 +22,7 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   Users user = UserPreferences.myUser;
-
+  late File file;
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: buildAppBar(context, "Settings"),
@@ -29,7 +34,11 @@ class _EditProfilePageState extends State<EditProfilePage> {
             ProfileWidget(
               imagePath: user.imagePath,
               isEdit: true,
-              onClicked: () async {},
+              onClicked: () async {
+                XFile? xfile =
+                    await ImagePicker().pickImage(source: ImageSource.gallery);
+                file = File(xfile!.path);
+              },
             ),
             const SizedBox(height: 24),
             TextFieldWidget(
@@ -53,4 +62,26 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ],
         ),
       );
+  updateProfile(BuildContext context) async {
+    Map<String, dynamic> map = Map();
+    if (file != null) {
+      String url = await uploadImage();
+      map['profileImage'] = url;
+    }
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update(map);
+    Navigator.pop(context);
+  }
+
+  Future<String> uploadImage() async {
+    TaskSnapshot taskSnapshot = await FirebaseStorage.instance
+        .ref()
+        .child("profile")
+        .child(
+            FirebaseAuth.instance.currentUser!.uid + "_" + basename(file.path))
+        .putFile(file);
+    return taskSnapshot.ref.getDownloadURL();
+  }
 }
