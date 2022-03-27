6b39191cf9fde3ba44990fe5523a17b589458787
Antoni <antekkrol1@gmail.com>
2022-03-28 00:01:17 +0200

    Bla bla bla

--- lib/utils/user_preferences.dart
+++ lib/utils/user_preferences.dart
@@ -1,29 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:io_project/model/user.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 User? user = FirebaseAuth.instance.currentUser;
 String? a = user!.displayName;
-String b = a!;
-String? c = user!.email;
-String d = c!;
+String? b = user!.email;
 
 class UserPreferences {
-  CollectionReference users =
-      FirebaseFirestore.instance.collection('usernames');
-  Stream docStream = FirebaseFirestore.instance
-      .collection('usernames')
-      .doc(FirebaseAuth.instance.currentUser?.uid)
-      .snapshots();
   static final myUser = Users(
-    name: b,
+    name: a!,
     imagePath:
         'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
-    email: d,
+    email: b!,
     about:
         'Certified Personal Trainer and Nutritionist with years of experience in creating effective diets and training plans focused on achieving individual customers goals in a smooth way.',
     isDarkMode: false,
